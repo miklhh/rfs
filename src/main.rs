c9f77aed049794c3b72b54dc3aaedc47561909a4
Mikael Henriksson <mike.zx@hotmail.com>
2021-12-06 21:13:37 +0100

    Exclude directory .git and all its children

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::io;
 use std::fs;
 use std::path::PathBuf;
 use std::collections::VecDeque;
+use regex::Regex;
+
+fn path_prefix_trim(dir_entry: &str) -> String  {
+    let re = Regex::new( r"^.*/" ).unwrap();
+    re.replace_all(dir_entry, "").to_string()
+}
 
 fn rfs_dir(dir: PathBuf) -> io::Result<()> {
     let mut fifo = VecDeque::new();
@@ -10,26 +16,33 @@ fn rfs_dir(dir: PathBuf) -> io::Result<()> {
         let current_dir = fifo.pop_front().unwrap();
         if current_dir.is_dir() {
 
-            // TODO: Permissions
+            // TODO: Print error on permission denied?
             let subdirs = match fs::read_dir(&current_dir) {
                 Ok(val) => val,
                 Err(_) => continue,
             };
-            
 
             for child_dir in subdirs {
                 let dir_entry = match child_dir {
                     Ok(val) => val.path(),
                     Err(_) => continue,
                 };
-                fifo.push_back(dir_entry);
+                
+                let dir_str = match dir_entry.to_str() {
+                    Some(val) => val,
+                    None => continue 
+                };
+
+                let trimmed_dir_str = path_prefix_trim(dir_str);
+                if trimmed_dir_str != ".git" {
+                    fifo.push_back(dir_entry);
+                }
             }
             println!("{}", current_dir.to_string_lossy());
         }
     }
     Ok(())
 }
-
 
 fn main() {
     rfs_dir(PathBuf::from(".")).unwrap();
